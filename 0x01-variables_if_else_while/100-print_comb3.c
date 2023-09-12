6c82aab53d4a49df7214cc9ed2b15f13659078e1
ayahhmeed <ayahhmeed@gmail.com>
2023-09-11 19:42:31 -0700

    tasl no10

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -4,29 +4,33 @@
 
 /**
  * main - Entry point
+ *
+ * Description: print all possible different combination of two digits.
+ *
  * Return: Alwyas 0 (success)
  */
 
 int main(void)
 {
-	int d, p;
+	int digit1 = 0, digit2;
 
-	for (d = '0'; d < '9'; d++)
+	while (digit1 <= 9)
 	{
-	for (p = d + 1; p <= '9'; p++)
-	{
-	if (p != d)
-	{
-	putchar(d);
-	putchar(p);
-	if (d == '8' && p == '9')
-	continue;
-	putchar(',');
-	putchar(' ');
-	}
-	}
-	}
-	ptuchar('\n');
-	return (0);
+		digit2 = 0;
+		while (digit2 <= 9)
+		{
+			if (digit != digit2 && digit1 < digit2)
+			{
+				putchar(digit1 + 48);
+				putchar(digit2 + 48);
+
+				if (digit1 + digit2 != 17)
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+			digit2++;
+
 }
 
