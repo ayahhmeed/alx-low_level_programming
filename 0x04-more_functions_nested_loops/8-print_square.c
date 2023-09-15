ca0390f56890e6e965b3e6d936262f43d36aef31
ayahhmeed <ayahhmeed@gmail.com>
2023-09-14 22:57:23 -0700

    taskno9

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -7,20 +7,21 @@
 
 void print_square(int size)
 {
+	int i, j;
+
 	if (size <= 0)
 	{
 		_putchar('\n');
 		return;
 	}
 
-	int i, j;
-
 	for (i = 0; i < size; i++)
 	{
 		for (j = 0; j < size; j++)
 			_putchar('#');
-
 		_putchar('\n');
 	}
+	_putchar('\n');
 }
+
 
