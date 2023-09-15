f89f4f106c6e3fd46edcfbd648b9a9ba46aa9d35
ayahhmeed <ayahhmeed@gmail.com>
2023-09-14 19:28:43 -0700

    task5

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -11,11 +11,10 @@ void more_numbers(void)
 
 	int num;
 
-	for (num= 0; num <= 9; x++)
+	for (num = 0; num <= 9; x++)
 	{
 	if (num == 2 || num == 4)
 	continue;
-	else
 	_putchar(num + 48);
 	}
 	_putchar('\n');
