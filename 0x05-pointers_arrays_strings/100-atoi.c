3e11352fd390f8c78ae641dda2dbfbb3ca97baf7
ayahhmeed <ayahhmeed@gmail.com>
2023-09-18 12:45:49 -0700

    task11

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -3,41 +3,30 @@
 
 /**
  * _atoi - converts a string to an integer
- * @s: the string to convert
- *
- * Return: the integer value of the string
+ * @s: the string to be converted
+ * 
+ * Return: integer value of the string
  */
 int _atoi(char *s)
 {
-unsigned int num = 0;
-int sign = 1;
+    int sign = 1;
+    unsigned int num = 0;
 
-while (*s == ' ')
-{
-s++;
-}
-if (*s == '-' || *s == '+')
-{
-if (*s == '-')
-{
-sign = -1;
-}
-s++;
-}
+    while (*s)
+    {
+        if (*s == '-')
+        {
+            sign = sign * -1;
+        }
 
-while (*s != '\0')
-{
-if (*s < '0' || *s > '9')
-{
-break;
-}
-if (num > (INT_MAX / 10))
-{
-return (sign == 1 ? INT_MAX : INT_MIN);
-}
-num = (num * 10) + (*s - '0');
-s++;
-}
+        if (*s >= '0' && *s <= '9')
+        {
+            num = num * 10 + (*s - '0');
+            if (*(s + 1) < '0' || *(s + 1) > '9')
+                break;
+        }
 
-return (num *sign);
+        s++;
+    }
+    return (num * sign);
 }
