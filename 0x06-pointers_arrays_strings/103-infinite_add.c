1fcd86996e8ce299df6cc91c71e586cd4837146a
ayahhmeed <ayahhmeed@gmail.com>
2023-09-21 18:41:22 -0700

    infinite

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -12,41 +12,31 @@
  */
 char *infinite_add(char *n1, char *n2, char *r, int size_r)
 {
-	int len1 = 0, len2 = 0, i = 0, j = 0, sum = 0, carry = 0;
+	int len1 = strlen(n1), len2 = strlen(n2), i, j, sum, carry = 0;
 
-	while (n1[len1] != '\0')
-		len1++;
-	while (n2[len2] != '\0')
-		len2++;
 	if (len1 >= size_r || len2 >= size_r)
-		return (0);
-	len1--;
-	len2--;
-	for (i = 0, j = 0; i <= len1 || j <= len2; i++, j++)
+		return (NULL);
+	n1 += len1 - 1;
+	n2 += len2 - 1;
+	for (i = 0, j = 0; i < len1 || j < len2 || carry; i++, j++)
 	{
-		if (i <= len1)
-			sum += n1[len1 - i] - '0';
-		if (j <= len2)
-			sum += n2[len2 - j] - '0';
-		sum += carry;
-		carry = sum >= 10;
+		sum = carry;
+		if (i < len1)
+			sum += *n1-- - '0';
+		if (j < len2)
+			sum += *n2-- - '0';
+		carry = sum / 10;
 		r[i] = sum % 10 + '0';
-		sum = 0;
 	}
-	if (carry && i == size_r - 1)
-		return (0);
-	if (carry)
+	if (i == size_r)
+		return (NULL);
+	r[i--] = '\0';
+	while (j <= i)
 	{
-		r[i++] = carry + '0';
-		carry = 0;
-	}
-	r[i] = '\0';
-	while (i-- > 0)
-	{
-		char tmp = r[j];
+		char c = r[j];
 
 		r[j++] = r[i];
-		r[i] = tmp;
+		r[i--] = c;
 	}
 	return (r);
 }
