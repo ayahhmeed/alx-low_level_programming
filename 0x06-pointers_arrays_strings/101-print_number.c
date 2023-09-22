1def274f91e9d66661178c0765c6556ab71ac424
ayahhmeed <ayahhmeed@gmail.com>
2023-09-21 18:57:17 -0700

    printnumber

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -8,21 +8,21 @@
  */
 void print_number(int n)
 {
-    unsigned int nb;
+unsigned int nb;
 
-    if (n < 0)
-    {
-        _putchar('-');
-        nb = -n;
-    }
-    else
-    {
-        nb = n;
-    }
+if (n < 0)
+{
+_putchar('-');
+nb = -n;
+}
+else
+{
+nb = n;
+}
 
-    if (nb / 10)
-        print_number(nb / 10);
+if (nb / 10)
+print_number(nb / 10);
 
-    _putchar(nb % 10 + '0');
+_putchar(nb % 10 + '0');
 }
 
