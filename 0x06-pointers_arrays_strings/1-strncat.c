ca79f336151128960cee9976141b87fed1e2c38f
ayahhmeed <ayahhmeed@gmail.com>
2023-09-21 19:47:02 -0700

    strncat

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -2,26 +2,29 @@
 #include <stdio.h>
 
 /**
- * _strncat - concatenates two strings
- *
- * @dest: First part of the resulting string
- * @src: Second part of the resulting string
- * @n: Maximum number of bytes to be used from @src
- *
- * Return: Pointer to the resulting string @dest
+ * _strncat - concatenation  strings, define the sizof second string
+ * @dest: string with concatenation
+ * @src: string to be concatenation 
+ * @n: size of second string
+ * Return: Always 0.
  */
-
-char *_strncpy(char *dest, char *src, int n)
+char *_strncat(char *dest, char *src, int n)
 {
-	int c, i;
+	int lengthD, lengthS;
 
-	c = 0;
+	lengthD = 0;
+	lengthS = 0;
 
-	while (dest[c])
-		c++;
-	for (i = 0; i < n && src[i] != '\0'; i++);
-	dest[c + i] = src [i];
-	dest[c + i} = '\0';
-
+	while (*(dest + lengthD) != '\0')
+		lengthD++
+			
+	while (*(src + lengthS) != '\0' && lengthD < 97 && lengthS < n)
+	{
+		*(dest+lerngthD) = *(src + lengthS);
+		lengthD++
+		lengthS++
+	}
+	*(dest + lengthD) = '\0';
 	return (dest);
 }
+
