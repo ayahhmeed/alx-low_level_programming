f324bd6edbdec6001984dfda75657696d6c42e00
ayahhmeed <ayahhmeed@gmail.com>
2023-09-25 17:59:54 -0700

    2strchr

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -8,17 +8,13 @@
  */
 char *_strchr(char *s, char c)
 {
-	while (*s != '\0')
+	while (*s)
 	{
 		if (*s == c)
-		{
 			return (s);
-		}
 		s++;
 	}
-	if (*s == c)
-	{
+	if (*s == '\0' && c == '\0')
 		return (s);
-	}
 	return (NULL);
 }
