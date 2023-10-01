e7719f30c6d7f990cadef81ca58f1952d394ad24
ayahhmeed <ayahhmeed@gmail.com>
2023-10-01 13:23:34 -0700

    no9

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -1,5 +1,4 @@
 #include "main.h"
-#include <string.h>
 
 /**
  * is_palindrome - Checks if a string is a palindrome
@@ -9,28 +8,40 @@
  */
 int is_palindrome(char *s)
 {
-    int len = strlen(s);
-    
-    return check_palindrome(s, len, 0);
+    int len = _strlen(s);
+    return check_palindrome(s, len, len - 1, 0);
 }
 
 /**
- * check_palindrome - Recursively checks if a string is a palindrome
+ * _strlen - Returns the length of a string
+ * @s: The string to get the length of
+ *
+ * Return: The length of s
+ */
+int _strlen(char *s)
+{
+    int len = 0;
+    if (s[len] == '\0')
+        return (0);
+    len += _strlen(s + 1);
+    return (len + 1);
+}
+
+/**
+ * check_palindrome - Recursive function that checks if a string is a palindrome
  * @s: The string to check
- * @len: The length of the string
- * @i: The current index to check
+ * @len: The length of s
+ * @i: The current starting index to check
+ * @j: The current ending index to check
  *
  * Return: 1 if s is a palindrome, 0 otherwise
  */
-int check_palindrome(char *s, int len, int i)
+int check_palindrome(char *s, int len, int i, int j)
 {
-    if (i < len/2)
-    {
-        if (s[i] != s[len - i - 1])
-            return 0;
-        
-        return check_palindrome(s, len, i+1);
-    }
-    
-    return 1;
+    if (i == len / 2)
+        return (1);
+    if (s[i] == s[j])
+        return (check_palindrome(s, len, i + 1, j - 1));
+    else
+        return (0);
 }
