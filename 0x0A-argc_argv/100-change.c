0840a5d8339bb25044f4aeda3efd1048467d7d8e
ayahhmeed <ayahhmeed@gmail.com>
2023-10-02 15:25:36 -0700

    thelast

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -10,44 +10,47 @@
  */
 int main(int argc, char *argv[])
 {
-  int num, j, result;
-  int coins[] = {25, 10, 5, 2, 1};
+int num, j, result;
+int coins[] = {25, 10, 5, 2, 1};
 
-  if (argc != 2)
-  {
-    printf("Error\n");
-    return 1;
-  }
+if (argc != 2)
+{
+printf("Error\n");
+return (1);
+}
 
-  num = atoi(argv[1]);
-  result = 0;
+num = atoi(argv[1]);
+result = 0;
 
-  if (num < 0)
-  {
-    printf("0\n");
-    return 0;
-  }
+if (num < 0)
+{
+printf("0\n");
+return (0);
+}
 
-  for (j = 0; j < 5 && num >= 0; j++)
-  {
-    while (num >= coins[j])
-    {
-      result++;
-      num -= coins[j];
-    }
-  }
+for (j = 0; j < 5 && num >= 0; j++)
+{
+while (num >= coins[j])
+{
+result++;
+num -= coins[j];
+}
+}
 
-  printf("%d\n", result);
-  return 0;
+printf("%d\n", result);
+return (0);
 }
 
 /*
-  Function: main
-  Description: This program takes in a single command line argument representing an amount of money and prints the minimum number of coins needed to make change for that amount.
+  * Function: main
+  * Description: This program takes in a single command line
+  * argument representing an amount of money and prints
+  * the minimum number of coins needed to make change for that amount.
   Parameters:
-    argc - an integer representing the number of command line arguments passed to the program
-    argv - an array of strings representing the command line arguments passed to the program
-  Return: an integer indicating whether the program completed successfully or not
+  * argc - an integer representing the number of
+  * command line arguments passed to the program
+  * argv - an array of strings representing the
+  * command line arguments passed to the program
+  * Return: an integer indicating whether
+  * the program completed successfully or not
 */
-
-
