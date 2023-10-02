295c5ee2e330cc5e0b643f6725fea84e1a4bb0e6
ayahhmeed <ayahhmeed@gmail.com>
2023-10-02 13:07:23 -0700

    tasno2

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * @argc: Number of command line arguments passed
+ * @argv: Array of command line arguments passed
+ *
+ * Return: Always 0 (Success)
+ */
+int main(int argc, char const *argv[])
+{
+  printf("%d\n", argc - 1);
+  return (0);
+}
+
