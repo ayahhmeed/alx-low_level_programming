d36fd9ddf4e7f110f87f42c2e4aa93328d889bcf
ayahhmeed <ayahhmeed@gmail.com>
2023-10-01 17:44:38 -0700

    taskno6

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -8,12 +8,11 @@
  */
 int _sqrt(int n, int i)
 {
-    if (i * i == n)
-        return (i);
-    else if (i * i > n)
-        return (-1);
-    
-    return (_sqrt(n, i + 1));
+if (i * i == n)
+return (i);
+else if (i * i > n)
+return (-1);
+return (_sqrt(n, i + 1));
 }
 
 /**
@@ -23,9 +22,8 @@ int _sqrt(int n, int i)
  */
 int _sqrt_recursion(int n)
 {
-    if (n < 0)
-        return (-1);
-    
-    return (_sqrt(n, 0));
+if (n < 0)
+return (-1);
+return (_sqrt(n, 0));
 }
 
