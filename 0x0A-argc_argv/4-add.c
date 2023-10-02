dfb58b3b549ff7355c5b2960e566ae55d283e097
ayahhmeed <ayahhmeed@gmail.com>
2023-10-02 14:58:35 -0700

    fourthtask

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -6,20 +6,22 @@
 /**
  * check_num - Check if a string is a number
  * @str: The string to check
- *
  * Return: 1 if the string is a number, 0 otherwise
  */
+
 int check_num(char *str)
 {
-  unsigned int count = 0;
+	unsigned int count = 0;
 
-  while (count < strlen(str)) {
-    if (!isdigit(str[count])) {
-      return 0;
-    }
-    count++;
-  }
-  return 1;
+	while (count < strlen(str))
+	{
+		if (!isdigit(str[count]))
+		{
+return (0);
+		}
+		count++;
+	}
+	return (1);
 }
 
 /**
@@ -29,36 +31,46 @@ int check_num(char *str)
  *
  * Return: Always 0 (Success)
  */
+
 int main(int argc, char *argv[])
 {
-  int count;
-  int sum = 0;
+	int count;
+	int sum = 0;
 
-  for (count = 1; count < argc; count++) {
-    if (check_num(argv[count])) {
-      sum += atoi(argv[count]);
-    } else {
-      printf("Error\n");
-      return 1;
-    }
-  }
-  printf("%d\n", sum);
-  return 0;
+	for (count = 1; count < argc; count++)
+	{
+		if (check_num(argv[count]))
+		{
+			sum += atoi(argv[count]);
+		}
+		else
+		{
+			printf("Error\n");
+			return (1);
+		}
+	}
+	printf("%d\n", sum);
+	return (0);
 }
 
 /*
-  Function: main
-  Description: This program takes in command line arguments and prints the sum of all valid numbers.
-  Parameters:
-    argc - an integer representing the number of command line arguments passed to the program
-    argv - an array of strings representing the command line arguments passed to the program
-  Return: an integer indicating whether the program completed successfully or not
+  * Function: main
+  * Description: This program takes in command line arguments and 
+  * prints the sum of all valid numbers.
+  * Parameters:
+    * argc - an integer representing the number of command line 
+    * arguments passed to the program
+    * argv - an array of strings representing the command line 
+    * arguments passed to the program
+   *Return: an integer indicating whether the program 
+   * completed successfully or not
 */
 
 /*
-  Function: check_num
-  Description: Checks if a string is a valid number.
-  Parameters:
-    str - the string to check
-  Return: 1 if the string is a number, 0 otherwise
-*/
+  * Function: check_num
+  * Description: Checks if a string is a valid number.
+  * Parameters:
+  * str - the string to check
+  * Return: 1 if the string is a number, 0 otherwise
+  */
+
