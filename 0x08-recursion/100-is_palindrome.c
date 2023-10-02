cbce646b97d20ffa942b5e300ee97b55f00b222c
ayahhmeed <ayahhmeed@gmail.com>
2023-10-01 18:06:43 -0700

    tasknumbe11

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -1,43 +1,57 @@
-#include "main.h"
+#include <stdio.h>
 
 /**
  * is_palindrome - Checks if a string is a palindrome recursively
  * @s: The string to check
- * @i: The start index of the string
- * @j: The end index of the string
- * Return: 1 if the string is a palindrome, 0 otherwise.
+ * @start: The start index of the substring to check
+ * @end: The end index of the substring to check
+ * Return: 1 if the substring is a palindrome, 0 otherwise.
  */
-int is_palindrome(char *s, int i, int j)
+int is_palindrome(char *s, int start, int end)
 {
-    if (i >= j)
-        return (1);
-    if (s[i] != s[j])
-        return (0);
-    
-    return (is_palindrome(s, i + 1, j - 1));
+    /* Base case: If the substring of length 0 or 1 is a palindrome, return 1 */
+    if (start >= end)
+        return 1;
+
+    /* Recursive case: If the first and last characters of the substring match 
+     * and the middle substring is also a palindrome, then the entire substring is a palindrome */
+    if (s[start] == s[end] && is_palindrome(s, start+1, end-1))
+        return 1;
+
+    /* If we reach here, the substring is not a palindrome */
+    return 0;
 }
 
 /**
- * _strlen - Calculates the length of a string recursively
- * @s: The string to calculate the length of
- * Return: The length of the string
- */
-int _strlen(char *s)
-{
-    if (*s == '\0')
-        return (0);
-    
-    return (1 + _strlen(s + 1));
-}
-
-/**
- * is_palindrome - Checks if a string is a palindrome
+ * is_palindrome_wrapper - Checks if a string is a palindrome
  * @s: The string to check
  * Return: 1 if the string is a palindrome, 0 otherwise.
  */
-int is_palindrome(char *s)
+int is_palindrome_wrapper(char *s)
 {
-    int len = _strlen(s);
+    int end = 0, start = 0;
 
-    return (is_palindrome(s, 0, len - 1));
+    /* Find the length of the string */
+    while (s[end] != '\0') {
+        end++;
+    }
+
+    /* Decrement the end index to exclude the null terminator */
+    end--;
+
+    /* Call is_palindrome() with the start and end indices of the string */
+    return is_palindrome(s, start, end);
+}
+
+/* Test the function */
+int main(void)
+{
+    /* Test cases */
+    printf("is_palindrome(\"abcddcba\") returns %d (expected 1)\n", is_palindrome_wrapper("abcddcba"));
+    
+    printf("is_palindrome(\"abcdcba\") returns %d (expected 1)\n", is_palindrome_wrapper("abcdcba"));
+    
+    printf("is_palindrome(\"I guess I thought the dream-space would be all about the visual, but it's more about the feeling. My question is what happens when you start messing with the physics of it.ti fo scisyhp eht htiw gnissem trats uoy nehw sneppah tahw si noitseuq yM .gnileef eht tuoba erom s'ti tub ,lausiv eht tuoba lla eb dluow ecaps-maerd eht thguoht I sseug I\") returns %d (expected 1)\n", is_palindrome_wrapper("I guess I thought the dream-space would be all about the visual, but it's more about the feeling. My question is what happens when you start messing with the physics of it.ti fo scisyhp eht htiw gnissem trats uoy nehw sneppah tahw si noitseuq yM .gnileef eht tuoba erom s'ti tub ,lausiv eht tuoba lla eb dluow ecaps-maerd eht thguoht I sseug I"));
+
+    return 0;
 }
