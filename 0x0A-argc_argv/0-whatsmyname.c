361cb192f8fcad11d38567dc595e0d28dd29ef9b
ayahhmeed <ayahhmeed@gmail.com>
2023-10-02 13:02:52 -0700

    task1

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,7 +1,15 @@
 #include <stdio.h>
 
-int main(int argc, char *argv[])
+/**
+ * main - print program name, followed by new line
+ * @argc: int
+ * @argv: list
+ * Return: 0
+ */
+
+int main(int argc, char const *argv[])
 {
+	(void)argc;
   printf("%s\n", argv[0]);
-  return 0;
+  return (0);
 }
