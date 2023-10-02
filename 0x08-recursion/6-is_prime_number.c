0b4258d3dbd17e4c30a1b3f86a69ba2c0df8b7dd
ayahhmeed <ayahhmeed@gmail.com>
2023-10-01 17:56:11 -0700

    taskno8

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -8,16 +8,14 @@
  */
 int is_prime(int n, int i)
 {
-    if (n <= 2)
-        return (n == 2);
-    if (n % i == 0)
-        return (0);
-    if (i * i > n)
-        return (1);
-    
-    return (is_prime(n, i + 1));
+if (n <= 2)
+return (n == 2);
+if (n % i == 0)
+return (0);
+if (i * i > n)
+return (1);
+return (is_prime(n, i + 1));
 }
-
 /**
  * is_prime_number - Checks if an input integer is prime
  * @n: The number to check
@@ -25,8 +23,7 @@ int is_prime(int n, int i)
  */
 int is_prime_number(int n)
 {
-    if (n <= 1)
-        return (0);
-    
-    return (is_prime(n, 2));
+if (n <= 1)
+return (0);
+return (is_prime(n, 2));
 }
