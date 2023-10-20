a70807fe0f760add6573a771a616ecc0c65ea8f4
ayahhmeed <ayahhmeed@gmail.com>
2023-10-19 18:03:54 -0700

    task2

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -9,19 +9,15 @@
  */
 list_t *add_node(list_t **head, const char *str)
 {
-    list_t *new;
-
-    if (head == NULL || str == NULL)
-        return (NULL);
-
-    new = malloc(sizeof(list_t));
-    if (new == NULL)
-        return (NULL);
-
-    new->str = strdup(str);
-    new->len = strlen(str);
-    new->next = *head;
-    *head = new;
-
-    return (new);
+list_t *new;
+if (head == NULL || str == NULL)
+return (NULL);
+new = malloc(sizeof(list_t));
+if (new == NULL)
+return (NULL);
+new->str = strdup(str);
+new->len = strlen(str);
+new->next = *head;
+*head = new;
+return (new);
 }
