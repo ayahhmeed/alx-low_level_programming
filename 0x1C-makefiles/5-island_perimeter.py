43491822e180f0e0f4944e2fe5adf914d22a7941
ayahhmeed <ayahhmeed@gmail.com>
2024-02-21 18:28:02 -0800

    make

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -1,27 +1,31 @@
 #!/usr/bin/python3
-"""
-Create a function def island_perimeter(grid):
- that returns the perimeter of the island
-"""
+"""Module that calculates the perimeter of an island in a grid."""
+
+
+def num_water_neighbors(grid, i, j):
+    """Returns the number of water neighbors a cell has in a grid."""
+
+    num = 0
+
+    if i <= 0 or not grid[i - 1][j]:
+        num += 1
+    if j <= 0 or not grid[i][j - 1]:
+        num += 1
+    if j >= len(grid[i]) - 1 or not grid[i][j + 1]:
+        num += 1
+    if i >= len(grid) - 1 or not grid[i + 1][j]:
+        num += 1
+
+    return num
+
+
 def island_perimeter(grid):
-    """ Function that returns the perimeter of islan""""
+    """Returns the perimeter of the island in grid."""
+
     perimeter = 0
-
-    numberRows = len(grid)
-
-    if grid != []:
-        numberColumns = len(grid[0])
-
-    for a in range(numberRows):
-        for b in range(numberColumns):
-            if grid[a][b] == 1:
-                if (a - 1) == -1 or grid[a - 1][b] == 0:
-                    perimeter += 1
-                if (a + 1) == numberRows or grid[a + 1][b] == 0:
-                    perimeter += 1
-                if (b - 1) == -1 or grid[a][b - 1] == 0:
-                    perimeter += 1
-                if (b + 1) == numberColumns or grid[a][b + 1] == 0:
-                    perimeter += 1
+    for i in range(len(grid)):
+        for j in range(len(grid[i])):
+            if grid[i][j]:
+                perimeter += num_water_neighbors(grid, i, j)
 
     return perimeter
